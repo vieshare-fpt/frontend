f1eeeb0c7d0d4b90a0fb4af8c269d0a7cb264d7c
huydeve <haihuy129@gmail.com>
2022-06-26 11:34:49 +0700

    Adjust the category section of the postcard

--- src/components/landing/components/post-card.js
+++ src/components/landing/components/post-card.js
@@ -3,6 +3,8 @@ import React from "react";
 import styles from "../../../styles/PostCard.module.css";
 import Typography from "@mui/material/Typography";
 import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
+import { Box } from "@mui/system";
+import { Stack } from "@mui/material";
 export function PostCards({ note }) {
   return (
     <>
@@ -24,38 +26,67 @@ export function PostCards({ note }) {
               )}
             </li>
             <li className={styles.views}>{note.views} lượt xem</li>
+            <li></li>
           </ul>
         </div>
 
         <div className={styles.description}>
-          <Link href={note.category.id}>
-            <Typography
-              variant="h4"
-              component="a"
-              sx={{ cursor: "pointer" }}
-              color="initial"
-            >
-              {note.category.name}
-              {note.type === "Premium" ? (
-                <span
-                  style={{
-                    fontSize: "12px",
-                    color: "#ffc107",
-                    lineHeight: "1px",
-                  }}
-                >
-                  <WorkspacePremiumIcon
-                    fontSize="medium"
-                    sx={{ color: "#ffc107" }}
-                  />
-                  Premium
-                </span>
-              ) : (
-                <></>
-              )}
-            </Typography>
-          </Link>
-          <h2>{note.title}</h2>
+          {/* <h1>Learning to Code</h1>
+          <h2>Opening a door to the future</h2>
+          <p>
+    
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum
+            dolorum architecto obcaecati enim dicta praesentium, quam nobis!
+            Neque ad aliquam facilis numquam. Veritatis, sit.
+          </p> */}
+          <Stack direction='row' spacing={1}>
+            <Link href={note.category.id}>
+              <Typography
+                variant="h2"
+                // component="h3"
+      
+              >
+                Thể loại | {note.category.name}
+              </Typography>
+            </Link>
+            {note.type === "Premium" ? (
+              <span
+                style={{
+                  fontSize: "10px",
+                  color: "#ffc107",
+                  lineHeight: "1px",
+                }}
+              >
+                <WorkspacePremiumIcon
+                  fontSize="medium"
+                  sx={{ color: "#ffc107" }}
+                />
+              </span>
+            ) : (
+              <></>
+            )}
+          </Stack>
+
+          <Typography
+            variant="h3"
+            sx={{
+              fontSize: "16px",
+              fontWeight: "bold",
+            }}
+            color="initial"
+          >
+            {note.title}
+          </Typography>
+
+          <Box
+            sx={{
+              backgroundColor: "#2f9f32",
+              width: "10%",
+              height: "4px",
+              borderRadius: "10px",
+              margin: "10px 0 10px 0",
+            }}
+          />
           <p>{note.description}</p>
           <div className={styles["read-more"]}>
             <Link href={`/post/${note.id}`}>
