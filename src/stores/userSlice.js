189f30738c88da476acccd538425309f617c6a15
huydeve <haihuy129@gmail.com>
2022-07-05 17:49:40 +0700

    refactor: remove some unnecessary reducers, change name reducer from "getUserInfo" to "setUserInfo"  and add "clearInfo" reducer to store state on logout

--- src/stores/userSlice.js
+++ src/stores/userSlice.js
@@ -1,71 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState: {
-        currentUserInfoFull: {
-            userInfo: null,
-            isFetching: false,
-            error: false,
-        },
-        currentUser: {
-            userInfo: null,
-            isFetching: false,
-            error: false,
-        },
-        currentUserInfoLimit: {
-            userInfo: null,
-            isFetching: false,
-            error: false,
-        }
+  name: "user",
+  initialState: {
+    currentUserInfoFull: {
+      userInfo: null,
+      isFetching: false,
+      error: false,
     },
-    reducers: {
-
-        getUserInfoStart: (state) => {
-            state.currentUser.isFetching = true;
-
-        },
-        getUserInfoSuccess: (state, action) => {
-            state.currentUser.isFetching = false;
-            state.currentUser.userInfo = action.payload;
-
-        },
-        getUserInfoFalse: (state) => {
-            state.currentUser.isFetching = false;
-            state.currentUser.error = true;
-        },
-
-        getUserInfoLimitStart: (state) => {
-            state.currentUserInfoLimit.isFetching = true;
-        },
-        getUserInfoLimitSuccess: (state, action) => {
-            state.currentUserInfoLimit.isFetching = false;
-            state.currentUserInfoLimit.userInfo = action.payload;
-
-        },
-        getUserInfoLimitFalse: (state) => {
-            state.currentUserInfoLimit.isFetching = false;
-            state.currentUserInfoLimit.error = true;
-        },
-        getUserInfoFullSuccess: (state, action) => {
-            state.currentUserInfoFull.userInfo = action.payload;
-        },
-        getUserInfoFullFalse: (state) => {
-            state.currentUserInfoLimit.isFetching = false;
-            state.currentUserInfoLimit.error = true;
-        },
-    }
-})
+    clearInfo: {
+      isFetching: false,
+      error: false,
+    },
+  },
+  reducers: {
+    setUserInfoSuccess: (state, action) => {
+      state.currentUserInfoFull.userInfo = action.payload;
+    },
+    setUserInfoFailed: (state) => {
+      state.currentUserInfoFull.isFetching = false;
+      state.currentUserInfoFull.error = true;
+    },
+    clearInfoStart: (state) => {
+      state.clearInfo.isFetching = true;
+    },
+    clearInfoSuccess: (state) => {
+      state.clearInfo.isFetching = false;
+      state.currentUserInfoFull.userInfo = null;
+      state.clearInfo.error = false;
+    },
+    clearInfoFailed: (state) => {
+      state.clearInfo.isFetching = false;
+      state.clearInfo.error = true;
+    },
+  },
+});
 
 export const {
-    getUserInfoStart,
-    getUserInfoSuccess,
-    getUserInfoFalse,
-    getUserInfoLimitStart,
-    getUserInfoLimitSuccess,
-    getUserInfoLimitFalse,
-    getUserInfoFullSuccess,
-    getUserInfoFullFalse
+  setUserInfoFailed,
+  setUserInfoSuccess,
+  clearInfoSuccess,
+  clearInfoFailed,
+  clearInfoStart,
 } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
