60bf9e3e68f6b8629e3cf6ccacf4579b8aa614fa
huydeve <haihuy129@gmail.com>
2022-07-05 11:12:10 +0700

    feat: add drawerSlide to store drawer state of navigation bar on desktop screen

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,12 +3,14 @@ import authReducer from "src/stores/authSlice";
 import userReducer from "src/stores/userSlice";
 import postReducer from "src/stores/postSlice";
 import categoryReducer from "src/stores/categorySlice";
+import drawerReducer from "src/stores/drawerSlice";
 
 const rootReducer = {
   auth: authReducer,
   user: userReducer,
   post: postReducer,
   category: categoryReducer,
+  drawer: drawerReducer,
 };
 
 const store = configureStore({
