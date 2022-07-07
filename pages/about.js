f6eaf5c734d5a0538ea25a17f462ae74972b022a
huydeve <haihuy129@gmail.com>
2022-07-07 18:23:24 +0700

    refactor: remove footer in about page

--- pages/about.js
+++ pages/about.js
@@ -162,28 +162,6 @@ export default function About() {
           </div>
         </div>
       </section>
-      <footer className={convert("bg-dark py-4 mt-auto")}>
-        <div className={convert("container px-5")}>
-          <div
-            className={convert(
-              "row align-items-center justify-content-between flex-column flex-sm-row"
-            )}
-          >
-            <div className={convert("col-auto")}>
-              <div className={convert("small m-0 text-white")}>
-                Copyright &copy; VieShare 2022
-              </div>
-            </div>
-            <div className={convert("col-auto")}>
-              <a href="#!">Privacy</a>
-              <span className={convert("text-white mx-1")}>&middot;</span>
-              <a href="#!">Terms</a>
-              <span className={convert("text-white mx-1")}>&middot;</span>
-              <a href="#!">Contact</a>
-            </div>
-          </div>
-        </div>
-      </footer>
     </>
   );
 }
