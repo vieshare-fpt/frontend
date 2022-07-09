fc0d5cfce291d817966427de166df7fe66ce1d02
huydeve <haihuy129@gmail.com>
2022-07-09 12:24:23 +0700

    chore: change action when error case is different status 401 or 400

--- src/utils/catchError.js
+++ src/utils/catchError.js
@@ -1,10 +1,10 @@
-import { clearInfoSuccess } from "src/stores/userSlice";
+import { clearInfoFailed, clearInfoSuccess } from "src/stores/userSlice";
 
 export default function catchError(error, dispatch, router) {
   if (error?.response?.status === 401 || error?.response?.status === 400) {
     router.push("/login");
     dispatch(clearInfoSuccess());
   } else {
-    dispatch(clearInfoSuccess());
+    dispatch(clearInfoFailed());
   }
 }
