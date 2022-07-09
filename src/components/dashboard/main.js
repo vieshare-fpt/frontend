31e1d51f6529daf79cc12c21f238b1c8d715bb09
huydeve <haihuy129@gmail.com>
2022-07-09 18:55:14 +0700

    fix: fix missing import lib "styled" of MUI

--- src/components/dashboard/main.js
+++ src/components/dashboard/main.js
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import TextSnippetIcon from '@mui/icons-material/TextSnippet';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import DesignServicesIcon from '@mui/icons-material/DesignServices';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import Link from 'next/link';
-
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import TextSnippetIcon from "@mui/icons-material/TextSnippet";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import Link from "next/link";
+import { styled } from "@mui/material";
 
 const drawerWidth = 240;
 
@@ -29,26 +29,34 @@ export default function ResponsiveDrawer(props) {
   };
 
   const subPages = [
-    { name: 'Bài viết của tôi', icon: TextSnippetIcon, link: '/dashboard/my-contents' },
-    { name: 'Thu nhập', icon: AttachMoneyIcon, link: '/dashboard/income' },
-    { name: 'Bản nháp', icon: DesignServicesIcon, link: '/dashboard/draft-contents' },
-    { name: 'Hồ sơ', icon: AccountBoxIcon, link: '/dashboard/writer-info' }
-  ]
+    {
+      name: "Bài viết của tôi",
+      icon: TextSnippetIcon,
+      link: "/dashboard/my-contents",
+    },
+    { name: "Thu nhập", icon: AttachMoneyIcon, link: "/dashboard/income" },
+    {
+      name: "Bản nháp",
+      icon: DesignServicesIcon,
+      link: "/dashboard/draft-contents",
+    },
+    { name: "Hồ sơ", icon: AccountBoxIcon, link: "/dashboard/writer-info" },
+  ];
 
   const MyLogo = styled(Typography)({
-    fontFamily: 'Salsa',
-    fontSize: '36px',
-    fontWeight: '400',
-    lineHeight: '44px',
-    letterSpacing: '0em',
-    textAlign: 'left',
+    fontFamily: "Salsa",
+    fontSize: "36px",
+    fontWeight: "400",
+    lineHeight: "44px",
+    letterSpacing: "0em",
+    textAlign: "left",
   });
 
   const drawer = (
     <div>
-      <Toolbar sx={{ display:"flex", justifyContent:"center" }}>
-        <MyLogo >
-          <Link href='/' sx={{ textDecoration:'auto', color: "forestgreen",}}>
+      <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
+        <MyLogo>
+          <Link href="/" sx={{ textDecoration: "auto", color: "forestgreen" }}>
             VieShare
           </Link>
         </MyLogo>
@@ -71,10 +79,11 @@ export default function ResponsiveDrawer(props) {
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -82,9 +91,7 @@ export default function ResponsiveDrawer(props) {
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
         }}
-      >
-
-      </AppBar>
+      ></AppBar>
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
@@ -98,8 +105,11 @@ export default function ResponsiveDrawer(props) {
             keepMounted: true,
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -107,15 +117,21 @@ export default function ResponsiveDrawer(props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
           {drawer}
         </Drawer>
       </Box>
-      <CurrentComponent handleDrawerToggle={handleDrawerToggle} props={props}></CurrentComponent>
+      <CurrentComponent
+        handleDrawerToggle={handleDrawerToggle}
+        props={props}
+      ></CurrentComponent>
     </Box>
   );
 }
