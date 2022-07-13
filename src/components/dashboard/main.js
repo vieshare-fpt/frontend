c44f0fd6f223964d1944278042e8a05c6cad0bda
huydeve <haihuy129@gmail.com>
2022-07-13 13:03:05 +0700

    style: add width of div containing Current Component to 100%

--- src/components/dashboard/main.js
+++ src/components/dashboard/main.js
@@ -164,7 +164,7 @@ export default function ResponsiveDrawer(props) {
           {drawer}
         </Drawer>
       </Box>
-      <Box>
+      <Box sx={{width: "100%"}}>
         <Toolbar/>
         <CurrentComponent
           handleDrawerToggle={handleDrawerToggle}
