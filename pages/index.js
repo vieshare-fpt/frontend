9f8e07cf65e251471c38d89ebfb436737a3d5155
huydeve <haihuy129@gmail.com>
2022-07-15 00:06:58 +0700

    fix: fix bug unable login by google at landing page

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,28 @@
-import { useGoogleOneTapLogin } from "@react-oauth/google";
 import LandingPage from "src/components/landing/pages/landing";
-import { getCookieData } from "src/services/cookies";
-import { accessApi, infoUserApi, categoryApi, postApi } from "src/services";
+import { categoryApi, postApi } from "src/services";
 import Page from "../src/components/landing/main";
-import { setCookieData } from "src/services/cookies";
 import { ReaderLayout } from "src/components/layouts";
 import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
-import { setUserInfoFailed, setUserInfoSuccess } from "src/stores/userSlice";
+import { useSelector } from "react-redux";
+
 
 export default function Landing(props) {
   const router = useRouter();
-  const dispatch = useDispatch();
-
   const user = useSelector(
-    (state) => state.persistedReducer.user?.currentUserInfoFull?.userInfo
+    (state) => state.persistedReducer.user.currentUserInfoFull.userInfo
   );
-
-  function getInfoUser(token, refreshToken) {
-    (async () => {
-      await infoUserApi
-        .info(token, refreshToken)
-        .then((response) => {
-          dispatch(setUserInfoSuccess(response.data));
-          router.push("/");
-        })
-        .catch(function (error) {
-          dispatch(setUserInfoFailed());
-        });
-    })();
-  }
-  if (!getCookieData("token")) {
-    useGoogleOneTapLogin({
-      onSuccess: (response) => {
-        const user = {
-          credential: response.credential,
-        };
-        (async () => {
-          await accessApi
-            .loginByGoogle(user)
-            .then(function (response) {
-              const token = response.data.token;
-              const refreshToken = response.data.refreshToken;
-              setCookieData("token", token);
-              setCookieData("refreshToken", refreshToken);
-              getInfoUser(token, refreshToken);
-            })
-            .catch(function (error) {
-              console.log(error.response.status); // 401
-            });
-        })();
-      },
-    });
-  }
   if (user !== null) {
     if (user.roles.includes("Admin") || user.roles.includes("Writer")) {
       router.push("/dashboard");
       return null;
-    } else if (response.data.roles.includes("Censor")) {
+    } else if (user.roles.includes("Censor")) {
       router.push("/censor");
       return null;
     } else {
       return <Page CurrentComponent={LandingPage} prop={props} />;
     }
   }
+  // return <Page CurrentComponent={{ props, history }} />;
   return <Page CurrentComponent={LandingPage} prop={props} />;
 }
 Landing.getLayout = ReaderLayout;
@@ -74,6 +33,7 @@ export async function getServerSideProps() {
     per_page: 12,
     page: 1,
   });
+
   const categories = await categoryApi.getCategory({
     per_page: 10,
     page: 1,
