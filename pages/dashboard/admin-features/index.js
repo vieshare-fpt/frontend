ee4283e62f009a50bc6375f7281a50c53c3c56d4
quevv <quevovan01062001@gmail.com>
2022-07-20 14:21:20 +0700

    feat: update users management function for admin

--- pages/dashboard/admin-features/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import AdminFeatures from 'src/components/dashboard/pages/admin-features'
-import Page from 'src/components/dashboard/main'
-
-export default function DashBoard() {
-  return <Page CurrentComponent={AdminFeatures}/>;
-}
