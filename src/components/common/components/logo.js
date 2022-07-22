6cc45a57f50ceaa0c0b2836d90bdec747ef039da
huydeve <haihuy129@gmail.com>
2022-07-22 23:45:25 +0700

    chore: add parameter for logo component

--- src/components/common/components/logo.js
+++ src/components/common/components/logo.js
@@ -1,23 +1,24 @@
 import { Typography } from "@mui/material";
+import { green } from "@mui/material/colors";
 import Link from "next/link";
 import React from "react";
 
-export default function Logo() {
+export default function Logo(props) {
+  const { size, lineHeight } = props;
   return (
-    <>
-      <Link href="/">
-        <Typography
-          component="a"
-          sx={{
-            color: "Green",
-            fontSize: "28px",
-            fontFamily: "Salsa",
-            cursor: "pointer",
-          }}
-        >
-          VieShare
-        </Typography>
-      </Link>
-    </>
+    <Link href="/">
+      <Typography
+        component="a"
+        sx={{
+          color: green[500],
+          fontSize: size,
+          lineHeight: lineHeight,
+          fontFamily: "Salsa",
+          cursor: "pointer",
+        }}
+      >
+        VieShare
+      </Typography>
+    </Link>
   );
 }
