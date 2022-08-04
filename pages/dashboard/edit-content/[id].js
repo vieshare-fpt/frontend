b5d6c537763d15e7cf4dcc44e619c30f501e6213
huydeve <haihuy129@gmail.com>
2022-08-05 01:06:54 +0700

    fix: handle check refresh token SSR

--- pages/dashboard/edit-content/[id].js
+++ pages/dashboard/edit-content/[id].js
@@ -1,27 +1,47 @@
-import React from 'react'
-import ContentEditor from 'src/components/dashboard/pages/content-editor'
-import Page from 'src/components/dashboard/main'
-import { postApi } from 'src/services'
+import React, { useEffect } from "react";
+import ContentEditor from "src/components/dashboard/pages/content-editor";
+import Page from "src/components/dashboard/main";
+import { postApi } from "src/services";
 import { categoryApi } from "src/services";
+import { clearInfoSuccess } from "src/stores/userSlice";
+import { useDispatch } from "react-redux";
+import { setCurrentCategory } from "src/stores/categorySlice";
+import { setTab } from "src/stores/tabSlice";
+import { removeCookieData } from "src/services/cookies";
 
 export default function EditContent(props) {
-    return <Page CurrentComponent={ContentEditor} props={props} />
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (props.initialPost === null) {
+      dispatch(clearInfoSuccess());
+      dispatch(setCurrentCategory(null));
+      dispatch(setTab("information"));
+      removeCookieData("token");
+      removeCookieData("refreshToken");
+      window.location.replace("/login");
+    }
+  }, [props.initialPost]);
+  return <Page CurrentComponent={ContentEditor} props={props} />;
 }
 
 export async function getServerSideProps(context) {
-    const { token, refreshToken } = context.req.cookies || {token: null, refreshToken: null}
-    const categories = await categoryApi.getCategory({
-        per_page: 99,
-        page:1
-      })
-    const postId = context.params?.id
-    if (!postId) return { notFound: true };
-    const response = await postApi.getPostDetail(postId, token, refreshToken);
-    console.log(response)
-    return {
-        props: {
-            initialPost: response.data,
-            categories: categories.data
-        },
-    };
+  const { token, refreshToken } = context.req.cookies || {
+    token: null,
+    refreshToken: null,
+  };
+  const categories = await categoryApi.getCategory({
+    per_page: 99,
+    page: 1,
+  });
+  const postId = context.params?.id;
+  if (!postId) return { notFound: true };
+  const response = await postApi.getPostDetail(postId, token, refreshToken);
+  console.log(response);
+  return {
+    props: {
+      initialPost: response.data ? response.data : response,
+      categories: categories.data,
+    },
+  };
 }
--- pages/dashboard/user-management/index.js
+++ pages/dashboard/user-management/index.js
@@ -2,8 +2,24 @@ import React from 'react'
 import UserManagement from 'src/components/dashboard/pages/user-management'
 import Page from 'src/components/dashboard/main'
 import { adminApi } from 'src/services';
+import { clearInfoSuccess } from 'src/stores/userSlice';
+import { setCurrentCategory } from 'src/stores/categorySlice';
+import { setTab } from 'src/stores/tabSlice';
+import { removeCookieData } from 'src/services/cookies';
+import { useDispatch } from 'react-redux';
 
 export default function DashBoard(props) {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (props.userData === null) {
+      dispatch(clearInfoSuccess());
+      dispatch(setCurrentCategory(null));
+      dispatch(setTab("information"));
+      removeCookieData("token");
+      removeCookieData("refreshToken");
+      window.location.replace("/login");
+    }
+  }, [props.userData]);
   return <Page CurrentComponent={UserManagement} 
                props={props}
   />;
@@ -12,10 +28,11 @@ export default function DashBoard(props) {
 export async function getServerSideProps(context) {
   const { token, refreshToken } = context.req?.cookies;
   const users = await adminApi.getUsers(token, refreshToken);
+
   return {
     props: {
       title: 'Quản lý người dùng',
-      userData : users.data,
+      userData : users.data ? users.data : users,
       token: token,
       refreshToken: refreshToken,
     },
